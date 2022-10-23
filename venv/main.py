093aa10d600f696ff8111e69ccc375ff16e52dca
tegbesemirone <oegbes2@uic.edu>
2022-10-23 11:48:53 -0500

    added functions to record Audio, and transcribe audio. Making a function for relaying audio as well, right now using the os system

---
+++ venv/main.py
@@ -0,0 +1,13 @@
+from speechtotext import *
+
+print("Hello")
+
+print("__name__ value: ", __name__)
+
+
+def main():
+    print("python main function")
+
+
+if __name__ == '__main__':
+    main()
--- venv/speechtotext.py
+++ venv/speechtotext.py
@@ -2,73 +2,69 @@ import pyaudio
 import wave
 import json
 import os
+import sounddevice as sd
+import soundfile as sf
 from google.cloud import speech
 from gtts import gTTS
 
 #initializes the cloud speech to text client
 client = speech.SpeechClient.from_service_account_file('key.json')
 
+samplerate = 44100  # Hertz
+duration = 5  # seconds
+filename = 'output.wav'
 
-#initializes the audio stream
-audio = pyaudio.PyAudio()
-stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
-frames = []
-
-# this loop records audio input from the user.
-# Needs to be updated to break out of loop without KeyboardInterrupt
-# Maybe we use an static timer that allots 5-7 seconds to respond
-try:
-    while True:
-        data = stream.read(1024)
-        frames.append(data)
-except KeyboardInterrupt:
-    print("KeyboardInterrupt")
-    pass
-
-#ends of audio recroding
-stream.stop_stream()
-stream.close()
-audio.terminate()
-
-#transcribes audio into .wav file
-sound_file = wave.open("myrecording.wav","wb")
-sound_file.setnchannels(1)
-sound_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
-sound_file.setframerate(44100)
-sound_file.writeframes(b''.join(frames))
-sound_file.close()
+#mydata = sd.rec(int(samplerate * duration), samplerate=samplerate,
+#                channels=2, blocking=True)
 
 
-#The following lines of code
-#chosses file name, from previous call
-file_name = "myrecording.wav"
+# function records audio for sentence analytics.
+# For now, this will have a static listening time of 5 seconds until
+# a more intuitive usage is found
+#returns type ndarray of sounddevice
+def recordAudio():
+    mydata = sd.rec(int(samplerate * duration), samplerate=samplerate,
+                    channels=2, blocking=True)
+    return mydata
 
-with open(file_name, 'rb') as f:
-    mp3_data = f.read()
+#this function accesses the google sppech to text api
+# then returns the transcript from the audio recording
+#returns a string
+def transcribeAudio(mydata, filename):
+    sf.write(filename, mydata, samplerate)
 
-audio_file = speech.RecognitionAudio(content=mp3_data)
+    with open(filename, 'rb') as f:
+        mp3_data = f.read()
 
-config = speech.RecognitionConfig(
-    sample_rate_hertz=44100,
-    enable_automatic_punctuation=True,
-    language_code='en-US'
-)
+    audio_file = speech.RecognitionAudio(content=mp3_data)
 
-response = client.recognize(
-    config=config,
-    audio=audio_file
-)
+    config = speech.RecognitionConfig(
+        sample_rate_hertz=44100,
+        enable_automatic_punctuation=True,
+        language_code='en-US',
+        audio_channel_count=2
+    )
 
-#This segment of code converts the json string to a json object
+    response = client.recognize(
+        config=config,
+        audio=audio_file
+    )
 
-json_file = type(response).to_json(response)
-wjdata = json.loads(json_file)
+    # This segment of code converts the json string to a json object
 
-#Static call to transcript will always be wjdata['results'][0]['alternatives'][0]['transcript']
-print(wjdata['results'][0]['alternatives'][0]['transcript'])
+    json_file = type(response).to_json(response)
+    wjdata = json.loads(json_file)
 
-#converts the text string from the transcript to mp3 audio
-myObj = gTTS(text=wjdata['results'][0]['alternatives'][0]['transcript'], lang='en', slow=False)
-myObj.save("tester.mp3")
+    # Static call to transcript will always be wjdata['results'][0]['alternatives'][0]['transcript']
+    transcript = str(wjdata['results'][0]['alternatives'][0]['transcript'])
 
-os.system("start tester.mp3")
+    # converts the text string from the transcript to mp3 audio
+    #myObj = gTTS(text=wjdata['results'][0]['alternatives'][0]['transcript'], lang='en', slow=False)
+    return transcript #return audio object
+
+
+
+
+#myObj.save("tester.mp3")
+
+#os.system("start tester.mp3")
