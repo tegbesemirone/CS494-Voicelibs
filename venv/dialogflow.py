5f4e2fed0afe4ecc3e488495d4bacef0a3e04199
tegbesemirone <oegbes2@uic.edu>
2022-11-26 13:45:05 -0600

    dialogflow integrationg
    So, we did a few things in this update. Most of the convertor logic was moved to the function textToAudio. Also, most of the logic for recording words was moved to transcribeAudio. Another function was implemented called callAgent, this connects to the dialogflowagent.
    
    Co-Authored-By: Rag <89797009+rsharm48@users.noreply.github.com>

--- venv/dialogflow.py
+++ venv/dialogflow.py
@@ -1,16 +1,31 @@
 from google.cloud import dialogflow
 from google.api_core.exceptions import InvalidArgument
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "dialogflow.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "venv\single-inquiry-369823-b1c6244e4daf.json"
 #credentials to connect to dialogflow API
 DIALOGFLOW_PROJECT_ID = 'single-inquiry-369823'
 DIALOGFLOW_LANGUAGE_CODE = 'en-US'
 #GOOGLE_APPLICATION_CREDENTIALS = 'dialogflow.json'
 SESSION_ID = 'madlibs-session'
 
+def callAgent(transcript):
+    agentResponse = ""
+    text_to_be_analyzed = transcript
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+    text_input = dialogflow.TextInput(text=text_to_be_analyzed, language_code=DIALOGFLOW_LANGUAGE_CODE)
+    query_input = dialogflow.QueryInput(text=text_input)
+    try:
+        response = session_client.detect_intent(session=session, query_input=query_input)
+        agentResponse = response.query_result.fulfillment_text
+    except InvalidArgument:
+        raise
+
+    return agentResponse
 
 
-text_to_be_analyzed = "Hi!"
+"""
+text_to_be_analyzed = "Hi, my name is Adam."
 session_client = dialogflow.SessionsClient()
 session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
 text_input = dialogflow.TextInput(text=text_to_be_analyzed, language_code=DIALOGFLOW_LANGUAGE_CODE)
@@ -23,3 +38,4 @@ print("Query text:", response.query_result.query_text)
 print("Detected intent:", response.query_result.intent.display_name)
 print("Detected intent confidence:", response.query_result.intent_detection_confidence)
 print("Fulfillment text:", response.query_result.fulfillment_text)
+"""
--- venv/main.py
+++ venv/main.py
@@ -3,24 +3,15 @@ from operator import le
 from re import I
 from numpy import empty
 from speechtotext import *
+from dialogflow import *
 from flask import *
-import pyttsx3
-import apiai as sr
-import speech_recognition as sr
+
 
 # Flask constructor takes the name of
 # current module (__name__) as argument.
 app = Flask(__name__)
 
-converter = pyttsx3.init()
-# Can be more than 100
-converter.setProperty('rate', 200)
-# Set volume 0-1
-converter.setProperty('volume', 0.5)
 
-r = sr.Recognizer()
-r.energy_threshold = 4000
-r.dynamic_energy_threshold = True
 
 
 currMadlib = ["Once upon a blank, in a kingdom far, far away ", ", there lived a blank princess. ",
@@ -41,58 +32,23 @@ wordAlt = ['n', 's', 'n', 'n', 'n', 's', 'n', 'n', 'n']
 @app.route('/')
 def main():
     gameOn = True
-    wrongRead = True
-    converter.say("Hello, what is your name?")
-    print("Hello, what is your name?")
-    converter.runAndWait()
-    # data
-    audio = 0
-    while wrongRead:
-        print("You may speak now......")
-        with sr.Microphone() as source:
-            r.adjust_for_ambient_noise(source, .25)
-            audio = r.listen(source)
-        # name = transcribeAudio(data, "output.wav")
-        try:
-            name = r.recognize_google(audio)
-            wrongRead = False
-        except:
-            print("Could Not Recognize what you said, try again")
-            converter.say("Could Not Recognize what you said, try again")
-            converter.runAndWait()
-    userName = name
-    converter.say("Hello " + name + " Welcome to Madlibs, a game where you can add your own words to finish the story")
-    print("Hello " + name + " Welcome to Madlibs, a game where you can add your own words to finish the story")
-    converter.say(
+    #wrongRead = True
+    textToAudio("Hello, what is your name?")
+    name = transcribeAudio()
+    userName = callAgent(name)
+    textToAudio("Hello " + userName + " Welcome to Madlibs, a game where you can add your own words to finish the story")
+    textToAudio(
         "While I dictate the story to you, I will add blanks in each section of the sentence for you to replace, then i will give you 3 seconds to fill the word")
-    print(
-        "While I dictate the story to you, I will add blanks in each section of the sentence for you to replace, then i will give you 3 seconds to fill the word")
-    converter.say(
+    textToAudio(
         "I'll also tell you what type of word i need, whether it's a noun, verb, or adjective. Do you understand? Say yes or help to continue.")
-    print(
-        "I'll also tell you what type of word i need, whether it's a noun, verb, or adjective. Do you understand? Say yes or help to continue.")
-    converter.runAndWait()
-    wrongRead = True
-    while wrongRead:
-        print("You may speak now......")
-        with sr.Microphone() as source:
-            r.adjust_for_ambient_noise(source, .25)
-            audio = r.listen(source)
-        try:
-            name = r.recognize_google(audio)
-            wrongRead = False
-        except:
-            print("Could Not Recognize what you said, try again")
-            converter.say("Could Not Recognize what you said, try again")
-            converter.runAndWait()
+    #converter.runAndWait()
+    name = transcribeAudio()
 
     if name == "Yes." or name == 'yes.':
-        converter.say("Great, let us begin")
-        print("Great, let us begin")
+        textToAudio("Great, let us begin")
     elif name == "Help." or name == "help.":
-        converter.say(helpPage())
-        print(helpPage())
-    converter.runAndWait()
+        textToAudio(helpPage())
+    #converter.runAndWait()
     # While loop runs on bool gameOn, will handle entire reading and analyzing of story
     index = 0
     while (gameOn):
@@ -103,68 +59,44 @@ def main():
             story = ""
             for a in newMadlib:
                 story += a
-            converter.say("You have reached the end of this passage, lets read your story and export")
-            print("You have reached the end of this passage, lets read your story and export")
-            converter.say(story)
-            print(story)
+            textToAudio("You have reached the end of this passage, lets read your story and export")
+            textToAudio(story)
             turnArrToPDF(newMadlib)
-            converter.say("Your story has been exported to a pdf, closing down the application")
-            print("Your story has been exported to a pdf, closing down the application")
-            converter.runAndWait()
+            textToAudio("Your story has been exported to a pdf, closing down the application")
+            #converter.runAndWait()
             os.close()
 
-        converter.say(currMadlib[index] + wordtype[index])
-        print(currMadlib[index] + wordtype[index])
-        converter.runAndWait()
+        textToAudio(currMadlib[index] + wordtype[index])
+        #converter.runAndWait()
         tobeValidated = True
 
         # While loop checks if a word needs to be validated, will handle error checking
         while (tobeValidated):
-            wrongRead = True
-            while wrongRead:
-                print("You may speak now......")
-                with sr.Microphone() as source:
-                    r.adjust_for_ambient_noise(source, .25)
-                    audio = r.listen(source)
-                try:
-                    name = r.recognize_google(audio)
-                    wrongRead = False
-                except:
-                    print("Could Not Recognize what you said, try again")
-                    converter.say("Could Not Recognize what you said, try again")
-                    converter.runAndWait()
-                    break
-            if wrongRead:
-                continue
+            name = transcribeAudio()
 
             if word_count(name) == 1:  # checks if only one word was stated
                 check = name.lower()
                 check = check.replace('.', '')
                 if (check == "help"):
-                    converter.say(helpPage())
-                    print(helpPage())
-                    converter.runAndWait()
+                    textToAudio(helpPage())
+                    #converter.runAndWait()
                     break
 
                 print("word is: " + check)
                 set = wordAnalyzer(check)
                 print(set)
-                if (wordMatch[index] in set or wordAlt[
-                    index] in set):  # checks if word matches neccessary requirements, appends index
+                if (wordMatch[index] in set or wordAlt[index] in set):  # checks if word matches neccessary requirements, appends index
                     newMadlib.append(currMadlib[index])
                     newMadlib[index] = newMadlib[index].replace("blank", check)
                     index += 1
                     tobeValidated = False
                 elif (len(set) == 0):
-                    converter.say(wordtype[index])
-                    print(wordtype[index])
-                    converter.runAndWait()
+                    textToAudio(wordtype[index])
+                    #converter.runAndWait()
                 else:
-                    converter.say("Sorry, that word wont work, say another one")
-                    print("Sorry, that word wont work, say another one")
-                    converter.say(wordtype[index])
-                    print(wordtype[index])
-                    converter.runAndWait()
+                    textToAudio("Sorry, that word wont work, say another one")
+                    textToAudio(wordtype[index])
+                    #converter.runAndWait()
 
 
             elif word_count(name) == 2:  # handles commands "Read Story" and "Finish Story"
@@ -174,31 +106,23 @@ def main():
                     story = ""
                     for a in newMadlib:
                         story += a
-                    converter.say(story)
-                    print(story)
-                    converter.say("I will now continue dictating the story from the next line")
-                    print("I will now continue dictating the story from the next line")
-                    converter.runAndWait()
+                    textToAudio(story)
+                    textToAudio("I will now continue dictating the story from the next line")
+                    #converter.runAndWait()
                     tobeValidated = False
                 if ('finish' in command and 'story' in command):  # finish story command
                     turnArrToPDF(newMadlib)
-                    converter.say(
+                    textToAudio(
                         "Story has been exported. I enjoyed your creativity " + userName + ", I am now shutting off goodbye.")
-                    print(
-                        "Story has been exported. I enjoyed your creativity " + userName + ", I am now shutting off goodbye.")
-                    converter.runAndWait()
+                    #converter.runAndWait()
                     gameOn = False
                     tobeValidated = False
             else:
-                converter.say(
+                textToAudio(
                     "I didn't understand what you just said, please keep phrases to one or two words until my creators improve my processing power." +
                     "Respond help to review the commands after I read the next line. I will now repeat the last mentioned line.")
-                converter.say(wordtype[index])
-                print(
-                    "I didn't understand what you just said, please keep phrases to one or two words until my creators improve my processing power." +
-                    "Respond help to review the commands after I read the next line. I will now repeat the last mentioned line.")
-                print(wordtype[index])
-                converter.runAndWait()
+                textToAudio(wordtype[index])
+                #converter.runAndWait()
 
 
 if __name__ == '__main__':
